97acfd18c235456ab13c06fd29a848a7cfd118cf
divya04032000 <diyadivya612@gmail.com>
2024-11-29 12:11:02 +0530

    day5

---
+++ Stringconcepts.java
@@ -0,0 +1,17 @@
+package day5;
+import java.util.Scanner;
+public class Stringconcepts {
+	public static void main(String args[])
+	{
+	
+	
+	
+	Scanner sc= new Scanner (System.in);
+	System.out.println("Enter your name");
+	String name=sc.nextLine();
+	System.out.println(name.charAt(4));
+	
+
+}
+}
+
