74de6f304a5f3e193623ed5a3cd0b4aa994f7c78
divya04032000 <diyadivya612@gmail.com>
2024-11-29 11:57:28 +0530

    Day-5

---
+++ Class.java
@@ -0,0 +1,34 @@
+package day5;
+
+public class Class {
+	 String name= "";
+	 int age= 0;
+	 int dob= 0;
+	 int rollNo= 0;
+	 
+	
+		public static void main(String args[])
+		{
+			Class student=new Class();
+			
+			student.name="Divya";
+			student.age=20;
+			student.dob=04-03-2000;
+			student.rollNo=961213207;
+			
+			System.out.println(student.name);
+			
+			
+            
+
+			Class student1=new Class();
+			
+			student1.name="Priya";
+			student1.age=22;
+			student1.dob=03-04-2001;
+			student1.rollNo=961213208;
+			
+			System.out.println(student1.name);
+			
+}
+}
